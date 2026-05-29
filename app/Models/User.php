0b1ba1f27f42025191db1cb3e896c864c281a58f
teelmarh <aneketelma123@gmail.com>
2026-05-29 16:26:50 +0100

    feat(User): add NIN fields, fillable, casts, and Notifiable

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,31 +2,52 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+    protected $fillable = [
+        'nin',
+        'first_name',
+        'last_name',
+        'middle_name',
+        'date_of_birth',
+        'gender',
+        'phone',
+        'nationality',
+        'birth_state',
+        'birth_lga',
+        'residence_state',
+        'residence_lga',
+        'residence_address',
+        'nin_photo',
+        'nin_verified',
+        'nin_verified_at',
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'nin_photo',
+    ];
+
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            'nin_verified_at'   => 'datetime',
+            'date_of_birth'     => 'date',
+            'nin_verified'      => 'boolean',
+            'password'          => 'hashed',
         ];
     }
 }
