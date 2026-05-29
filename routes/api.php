b10a35902251c13e082888eacb329d2b8329859e
teelmarh <aneketelma123@gmail.com>
2026-05-29 16:29:05 +0100

    feat(routes): update api routes

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,22 @@
 <?php
 
+use App\Http\Controllers\Api\V1\Users\Auth\ForgotPasswordController;
+use App\Http\Controllers\Api\V1\Users\Auth\LoginController;
+use App\Http\Controllers\Api\V1\Users\Auth\RegisterController;
+use App\Http\Controllers\Api\V1\Users\Auth\ResetPasswordController;
+use App\Http\Controllers\Api\V1\Users\Auth\SendTokenController;
+use App\Http\Controllers\Api\V1\Users\Auth\VerifyEmailController;
 use App\Http\Controllers\Api\V1\System\HealthController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/health', [HealthController::class, 'index']);
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(function() {
+    Route::apiResource('/health', HealthController::class)->only('index');    // Route::apiResource('/register', RegisterController::class)->only('store');
+    Route::apiResource('/login', LoginController::class)->only('store');
+    Route::apiResource('/send-token', SendTokenController::class)->only('store');
+    Route::apiResource('/verify-email', VerifyEmailController::class)->only('store');
+    Route::apiResource('/reset-password', ResetPasswordController::class)->only('store');
+    Route::apiResource('/forgot-password', ForgotPasswordController::class)->only('store');
+});
+
+
