81adb3af83d67f7e9b94bf8825585e0b3419e149
teelmarh <aneketelma123@gmail.com>
2026-05-30 15:26:31 +0100

    fix(routes)

--- routes/api.php
+++ routes/api.php
@@ -11,12 +11,13 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('v1')->group(function() {
-    Route::apiResource('/health', HealthController::class)->only('index');    // Route::apiResource('/register', RegisterController::class)->only('store');
+    Route::apiResource('/health', HealthController::class)->only('index');
+
+    Route::apiResource('/register', RegisterController::class)->only('store');
     Route::apiResource('/login', LoginController::class)->only('store');
     Route::apiResource('/send-token', SendTokenController::class)->only('store');
     Route::apiResource('/verify-email', VerifyEmailController::class)->only('store');
     Route::apiResource('/reset-password', ResetPasswordController::class)->only('store');
     Route::apiResource('/forgot-password', ForgotPasswordController::class)->only('store');
 });
-
 
